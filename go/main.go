aac35765afab5de704bd5cd6b4b26bd93a9a792a
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-11-16 18:19:52 +0900

    perf: グラフ遅くね

--- go/main.go
+++ go/main.go
@@ -804,7 +804,12 @@ func generateIsuGraphResponse(tx *sqlx.Tx, jiaIsuUUID string, graphDate time.Tim
 	var startTimeInThisHour time.Time
 	var condition IsuCondition
 
-	rows, err := tx.Queryx("SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY `timestamp` ASC", jiaIsuUUID)
+	endTime := graphDate.Add(time.Hour * 24)
+	rows, err := tx.Queryx("SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? AND `timestamp` >= ? AND `timestamp` < ? ORDER BY `timestamp` ASC",
+		jiaIsuUUID,
+		graphDate,
+		endTime,
+	)
 	if err != nil {
 		return nil, fmt.Errorf("db error: %v", err)
 	}
@@ -855,7 +860,6 @@ func generateIsuGraphResponse(tx *sqlx.Tx, jiaIsuUUID string, graphDate time.Tim
 			})
 	}
 
-	endTime := graphDate.Add(time.Hour * 24)
 	startIndex := len(dataPoints)
 	endNextIndex := len(dataPoints)
 	for i, graph := range dataPoints {
